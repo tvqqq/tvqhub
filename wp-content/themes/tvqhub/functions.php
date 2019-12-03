dd12c831722676084762d5e078d377b6ea338181
Tat Vi Quyen <tvq9612@gmail.com>
2019-12-03 20:55:16 +0700

    init theme tvqhub

---
+++ wp-content/themes/tvqhub/functions.php
@@ -0,0 +1,94 @@
+<?php
+
+if (!function_exists('tvqhub_setup')) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function tvqhub_setup()
+    {
+        // Hide admin bar
+        add_filter('show_admin_bar', '__return_false');
+
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on tvqhub, use a find and replace
+         * to change 'tvqhub' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('tvqhub', get_template_directory() . '/languages');
+
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
+
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
+
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
+
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(array(
+            'primary' => __('Primary Menu'),
+        ));
+
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support('html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
+
+        // Set up the WordPress core custom background feature.
+        add_theme_support('custom-background', apply_filters('tvqhub_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        )));
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
+    }
+endif;
+add_action('after_setup_theme', 'tvqhub_setup');
+
+/**
+ * Enqueue scripts and styles.
+ */
+add_action('wp_enqueue_scripts', 'tvqhub_enqueue_scripts');
+function tvqhub_enqueue_scripts()
+{
+    // Base css & js
+    wp_enqueue_style('tvqhub-css', get_stylesheet_uri());
+    wp_enqueue_script('tvqhub-js', get_template_directory_uri() . '/js/script.js');
+}
+
+/**
+ * Javascript for old browser IE 8,7,6.
+ */
+add_action('wp_head', 'ie_scripts');
+function ie_scripts()
+{
+    echo '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->';
+    echo '<!-- WARNING: Respond.js doesn\'t work if you view the page via file:// --> ';
+    echo '<!--[if lt IE 9]>';
+    echo '<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>';
+    echo '<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
+    echo '<![endif]-->';
+}
