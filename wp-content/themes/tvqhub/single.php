73c9ecea3c49eeb5580506c569f93442416e52cf
Tat Vi Quyen <tvq9612@gmail.com>
2019-12-06 07:40:50 +0700

    single.php

---
+++ wp-content/themes/tvqhub/single.php
@@ -0,0 +1,26 @@
+<?php
+get_header();
+?>
+
+    <div class="container">
+    <div class="row">
+    <div id="primary" class="content-area col-12 col-lg-9">
+        <main id="main" class="site-main" role="main">
+            <?php
+            while (have_posts()) : the_post();
+                get_template_part('template-parts/posts/detail', get_post_format());
+                the_post_navigation();
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) :
+                    comments_template();
+                endif;
+
+            endwhile; // End of the loop.
+            ?>
+        </main>
+    </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
---
+++ wp-content/themes/tvqhub/template-parts/posts/detail.php
@@ -0,0 +1,30 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="card card-post-detail px-2 mb-5">
+        <div class="card-body">
+            <div class="arrow"></div>
+            <h2 class="entry-title"><?php echo get_the_title() ?></h2>
+
+            <div class="entry-meta mb-3">
+                <time class="entry-date"
+                      datetime="<?php echo esc_attr(get_the_date('c')) ?>"><?php echo esc_attr(get_the_date()) ?></time>
+                <span>•</span>
+                <?php $category = get_the_category(get_the_ID())[0];
+                $tagsList = get_the_tag_list(null, ','); ?>
+                <a href="<?php echo esc_url(home_url('/category/') . $category->slug) ?>"><?php echo $category->name ?></a>
+                <?php if ($tagsList) { ?>
+                    • <i class="fas fa-tags"></i>
+                    <span class="tags-links"><?php echo $tagsList ?></span>
+                <?php } ?>
+            </div>
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+
+            <div class="entry-footer">
+                <!--TODO: related post, post count,...-->
+            </div>
+        </div>
+    </div>
+</article>
+<!-- #post-## -->
