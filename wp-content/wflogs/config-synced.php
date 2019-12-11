f8dc0259d1a3e162f8f08b8e08f0c1de218094b3
Tat Vi Quyen <tvq9612@gmail.com>
2019-12-11 21:11:23 +0700

    update

--- wp-content/wflogs/config-synced.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:19:{s:6:"apiKey";s:128:"2808b74913341ff61cac71777a275624a9352e69624972732a421ef689744bc199d3a60732dc7f61dba87976619282df7bc2e72ca0fac5794cc6095edb7afcfa";s:6:"isPaid";b:0;s:7:"siteURL";N;s:7:"homeURL";N;s:14:"whitelistedIPs";N;s:9:"howGetIPs";N;s:25:"howGetIPs_trusted_proxies";N;s:13:"pluginABSPATH";N;s:11:"other_WFNet";b:1;s:9:"serverIPs";N;s:15:"blockCustomText";N;s:13:"timeoffset_wf";N;s:23:"advancedBlockingEnabled";N;s:21:"betaThreatDefenseFeed";N;s:20:"disableWAFIPBlocking";N;s:13:"patternBlocks";N;s:13:"countryBlocks";N;s:11:"otherBlocks";N;s:8:"lockouts";N;}
--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1576674372;s:7:"authKey";s:64:"3TEw&!?Y #.+%B#Zyc8WeJ_(stO|tZc#|7 th~QOVgi}L;(-LPBA/h(}E4.gSQhv";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:457;}
