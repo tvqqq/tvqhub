825330909243b58332b13b5cec457526e504812c
Tat Vi Quyen <tvq9612@gmail.com>
2019-12-14 21:20:17 +0700

    plugin itheme

---
+++ wp-content/plugins/better-wp-security/dist/manifest.php
@@ -0,0 +1,108 @@
+<?php return array (
+  'core/admin-notices' => 
+  array (
+    'runtime' => true,
+    'files' => 
+    array (
+      0 => 'core/admin-notices.css',
+      1 => 'core/admin-notices.js',
+    ),
+    'hash' => '9d0cac63105d6f0bcc30d06f969d8be7',
+    'contentHash' => 
+    array (
+      'css/mini-extract' => 'd5d74d576a086d437a59',
+      'javascript' => '78a2c380e8a93d8e10f7',
+    ),
+    'vendors' => 
+    array (
+    ),
+    'dependencies' => 
+    array (
+      0 => '@ithemes/security.core.admin-notices-api',
+      1 => 'lodash',
+      2 => 'wp-autop',
+      3 => 'wp-components',
+      4 => 'wp-compose',
+      5 => 'wp-data',
+      6 => 'wp-dom-ready',
+      7 => 'wp-element',
+      8 => 'wp-i18n',
+    ),
+  ),
+  'core/admin-notices-api' => 
+  array (
+    'runtime' => true,
+    'files' => 
+    array (
+      0 => 'core/admin-notices-api.js',
+    ),
+    'hash' => '4830a04fca439f907049c335efb143f9',
+    'contentHash' => 
+    array (
+      'css/mini-extract' => '31d6cfe0d16ae931b73c',
+      'javascript' => 'f64242c0f2acc73e4f26',
+    ),
+    'vendors' => 
+    array (
+    ),
+    'dependencies' => 
+    array (
+      0 => 'lodash',
+      1 => 'wp-api-fetch',
+      2 => 'wp-data',
+      3 => 'wp-i18n',
+    ),
+  ),
+  'core/admin-notices-dashboard-admin-bar' => 
+  array (
+    'runtime' => true,
+    'files' => 
+    array (
+      0 => 'core/admin-notices-dashboard-admin-bar.css',
+      1 => 'core/admin-notices-dashboard-admin-bar.js',
+    ),
+    'hash' => 'a95fc2fb1e30fed9c7938f08c12b6d7b',
+    'contentHash' => 
+    array (
+      'css/mini-extract' => '732388055deb8942488c',
+      'javascript' => 'b6d1b12859cd32d64740',
+    ),
+    'vendors' => 
+    array (
+    ),
+    'dependencies' => 
+    array (
+      0 => '@ithemes/security.core.admin-notices-api',
+      1 => '@ithemes/security.dashboard.api',
+      2 => 'lodash',
+      3 => 'wp-autop',
+      4 => 'wp-components',
+      5 => 'wp-compose',
+      6 => 'wp-data',
+      7 => 'wp-element',
+      8 => 'wp-i18n',
+      9 => 'wp-plugins',
+    ),
+  ),
+  'core/packages/components/site-scan-results/style' => 
+  array (
+    'runtime' => true,
+    'files' => 
+    array (
+      0 => 'core/packages/components/site-scan-results/style.css',
+      1 => 'core/packages/components/site-scan-results/style.js',
+    ),
+    'hash' => '17241399d68eeb900f5f2c289b1748f7',
+    'contentHash' => 
+    array (
+      'css/mini-extract' => '4938d7aee319aaa3968c',
+      'javascript' => '0ac75a114cf101452605',
+    ),
+    'vendors' => 
+    array (
+    ),
+    'dependencies' => 
+    array (
+    ),
+  ),
+);
