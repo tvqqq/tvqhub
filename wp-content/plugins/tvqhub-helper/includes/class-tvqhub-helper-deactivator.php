78ab5b807d505ec644b4ab894c2c90f2524229b7
Tat Vi Quyen <tvq9612@gmail.com>
2020-04-25 21:42:15 +0700

    [tvqhub-helper] init

---
+++ wp-content/plugins/tvqhub-helper/includes/class-tvqhub-helper-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://tvqhub.com
+ * @since      1.0.0
+ *
+ * @package    Tvqhub_Helper
+ * @subpackage Tvqhub_Helper/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Tvqhub_Helper
+ * @subpackage Tvqhub_Helper/includes
+ * @author     TVQ <quyen@tvqhub.com>
+ */
+class Tvqhub_Helper_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
