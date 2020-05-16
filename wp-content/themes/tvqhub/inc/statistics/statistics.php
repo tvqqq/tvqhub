e8e560f29b00e2ba775e3aac129fef012589bc3f
Tat Vi Quyen <tvq9612@gmail.com>
2020-05-16 07:38:51 +0700

    round thousand statistic

--- wp-content/themes/tvqhub/inc/statistics/statistics.php
+++ wp-content/themes/tvqhub/inc/statistics/statistics.php
@@ -7,7 +7,7 @@ function tvqhub_statistics()
         'since' => $since,
         'days' => tvqhub_stat_get_days($since),
         'posts' => wp_count_posts()->publish,
-        'views' => PostViewModel::getTotalViews(),
+        'views' => _roundThousand(PostViewModel::getTotalViews()),
     ];
 }
 
@@ -16,4 +16,20 @@ function tvqhub_stat_get_days($since)
     $startDate = date_create($since);
     $today = date_create(date('Y-m-d'));
     return date_diff($startDate, $today)->days;
+}
+
+function _roundThousand($num)
+{
+    if ($num > 1000) {
+        $x = round($num);
+        $x_number_format = number_format($x);
+        $x_array = explode(',', $x_number_format);
+        $x_parts = array('k', 'm', 'b', 't');
+        $x_count_parts = count($x_array) - 1;
+        $x_display = $x;
+        $x_display = $x_array[0] . ((int)$x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
+        $x_display .= $x_parts[$x_count_parts - 1];
+        return $x_display;
+    }
+    return $num;
 }
