11a924698dd6a2ce7265b16133986a53a07e00fc
Tat Vi Quyen <tvq9612@gmail.com>
2020-07-10 21:35:10 +0700

    update plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5f6b4390fe0b9f0860b977d3dd766580::getLoader();
+return ComposerAutoloaderInitdd0445b5872c5410d0589a6bf92afbc1::getLoader();
